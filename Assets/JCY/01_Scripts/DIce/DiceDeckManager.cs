66e3f386ab9c94c87e9bec9c396c03f344281ecb
seonmulsangja <opqrstuvw0156@gmail.com>
2026-08-19 23:48:41 +0900

    다이스 덱 보완

--- Assets/JCY/01_Scripts/DIce/DiceDeckManager.cs
+++ Assets/JCY/01_Scripts/DIce/DiceDeckManager.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DiceDeckManager : MonoBehaviour
 {
     // 내가 보유한 전체 주사위
     [SerializeField] private List<DiceSO_JCY> diceCollection;
+    
+    // 기본 덱에 들어가는 주사위
+    [SerializeField] private DiceSO_JCY defaultDice;
 
     // 현재 덱에 들어있는 주사위
     [SerializeField] private List<DiceSO_JCY> diceDeck;
 
     // 이번 턴에 뽑힌 주사위
     [SerializeField] private List<DiceSO_JCY> drawnDice;
+    
+    //버려진 주사위들
+    [SerializeField] private List<DiceSO_JCY> discardPile;
 
     // 한 턴에 뽑을 주사위 개수
     [SerializeField] private int drawCount = 6;
@@ -29,11 +36,42 @@ public class DiceDeckManager : MonoBehaviour
         }
     }
     
-    public void DrawDiceAndStartTurn()
+    private void Start()
     {
-        // 1. 덱에서 주사위 뽑기
-        
-        drawnDice.Clear();
+        InitializeDeck();
+    }
+
+    private void InitializeDeck()
+    {
+        diceDeck.Clear();
+
+        for (int i = 0; i < 10; i++)
+        {
+            diceDeck.Add(defaultDice);
+        }
+
+        ShuffleDeck();
+    }
+
+    //기본 카드 섞기
+    private void ShuffleDeck()
+    {
+        for (int i = 0; i < diceDeck.Count; i++)
+        {
+            int randomIndex = Random.Range(i, diceDeck.Count);
+
+            DiceSO_JCY temp = diceDeck[i];
+            diceDeck[i] = diceDeck[randomIndex];
+            diceDeck[randomIndex] = temp;
+        }
+    }
+
+    //덱에서 주사위 뽑아서 전달
+    public void DrawDice()
+    {
+        if(diceDeck.Count < drawCount)
+            ReshuffleDeck();
+        DiscardDice(); 
         for (int i = 0; i < drawCount; i++)
         {
             int randomIndex = Random.Range(0, diceDeck.Count);
@@ -42,7 +80,7 @@ public class DiceDeckManager : MonoBehaviour
             DiceSO_JCY selectedDice = diceDeck[randomIndex];
 
             drawnDice.Add(selectedDice);
-
+            
             diceDeck.RemoveAt(randomIndex);
         }
 
@@ -50,8 +88,27 @@ public class DiceDeckManager : MonoBehaviour
         DiceManager_JCY.Instance.StartTurn(drawnDice);
     }
 
-    public void ClearDeck()
+    //다이스 버린거 버리는 덱에 넣고 드로우한 다이스 초기화
+    public void DiscardDice()
     {
-        
+        discardPile.AddRange(drawnDice);
+        drawnDice.Clear();
+    }
+    
+    //덱에 있는 다이스가 6개 이하면 보충해주는 함수
+    public void ReshuffleDeck()
+    {
+        diceDeck.AddRange(discardPile);
+        discardPile.Clear();
+
+        // 여기서 랜덤하게 섞기
+        for (int i = 0; i < diceDeck.Count; i++)
+        {
+            int randomIndex = Random.Range(i, diceDeck.Count);
+
+            DiceSO_JCY temp = diceDeck[i];
+            diceDeck[i] = diceDeck[randomIndex];
+            diceDeck[randomIndex] = temp;
+        }
     }
 }
