59f73ed81f008e41c18e442e566cc5fa4c6a211b
press101-spare <jeonjibin710@gmail.com>
2026-08-24 17:10:33 +0900

    ㅇㅇ

---
+++ Assets/JJB/Script/Dice/JJB_RandomDiceRoller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace JJB.Script
+{
+    public class JJB_RandomDiceRoller : MonoBehaviour, IDiceRoller
+    {
+        public int Roll()
+        {
+            return Random.Range(1, 7);
+        }
+    }
+}
