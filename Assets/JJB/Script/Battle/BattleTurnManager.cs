748d3e549d0e9e7429b5000c7f3f731a66d98565
press101-spare <jeonjibin710@gmail.com>
2026-08-29 17:08:29 +0900

    턴제 시스템 만듬

---
+++ Assets/JJB/Script/Battle/BattleTurnManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace JJB.Script.Battle
+{
+    public class BattleTurnManager : MonoBehaviour
+    {
+        [SerializeField] private EnemyTurnController enemyTurnController;
+
+        private BattleTurn _currentTurn;
+
+        public BattleTurn CurrentTurn => _currentTurn;
+
+        public event Action<BattleTurn> OnTurnChanged;
+
+        private void Start()
+        {
+            StartPlayerTurn();
+        }
+
+        private void StartPlayerTurn()
+        {
+            _currentTurn = BattleTurn.Player;
+
+            OnTurnChanged?.Invoke(_currentTurn);
+        }
+
+        public void EndPlayerTurn()
+        {
+            if (_currentTurn != BattleTurn.Player)
+                return;
+
+            StartEnemyTurn();
+        }
+
+        private void StartEnemyTurn()
+        {
+            _currentTurn = BattleTurn.Enemy;
+
+            OnTurnChanged?.Invoke(_currentTurn);
+
+            enemyTurnController.ExecuteTurn(
+                EndEnemyTurn
+            );
+        }
+
+        private void EndEnemyTurn()
+        {
+            StartPlayerTurn();
+        }
+
+        public void EndBattle()
+        {
+            _currentTurn = BattleTurn.BattleEnd;
+
+            OnTurnChanged?.Invoke(_currentTurn);
+        }
+    }
+}
---
+++ Assets/JJB/Script/Battle/EnemyHealthSetup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace JJB.Script.Battle
+{
+    [RequireComponent(typeof(Health))]
+    public class EnemyHealthSetup : MonoBehaviour
+    {
+        [SerializeField] private EnemyData enemyData;
+
+        public EnemyData Data => enemyData;
+
+        private void Awake()
+        {
+            GetComponent<Health>().Initialize(enemyData.MaxHealth);
+        }
+    }
+}
