70933ab30da4cdfc40b2a1da4bb9b01b83824b18
press101-spare <jeonjibin710@gmail.com>
2026-09-07 08:54:24 +0900

    ss

--- Assets/JJB/Script/Battle/PlayerAttackController.cs
+++ Assets/JJB/Script/Battle/PlayerAttackController.cs
@@ -20,28 +20,41 @@ namespace JJB.Script.Battle
 
             if (baseDamage <= 0)
                 return;
-            
-            // 나중에 주사위 특수효과 계산할 위치
 
             int finalDamage = baseDamage;
-
-            // 출혈 수치 계산
-            // 흡혈 수치 계산
-            
-            // 실제 적에게 피해
 
             enemyJjbHealth.TakeDamage(finalDamage);
 
             Debug.Log(
                 $"{treeType} 공격 / 최종 피해 : {finalDamage}"
             );
-            
-            // 나중에 공격 후 효과
+        }
+        
+        public void Attack(int baseDamage)
+        {
+            if (enemyJjbHealth == null)
+            {
+                Debug.LogError("Enemy Health가 연결되지 않았습니다.");
+                return;
+            }
 
-            // 출혈 부여
-            // 흡혈 회복
-            // 실드 획득
-            // 재굴림 획득
+            if (enemyJjbHealth.IsDead)
+                return;
+
+            if (baseDamage <= 0)
+                return;
+
+            int finalDamage = baseDamage;
+
+            // 나중에 여기서
+            // 출혈
+            // 흡혈
+            // 최종 데미지 증가
+            // 주사위 특수효과 계산
+
+            enemyJjbHealth.TakeDamage(finalDamage);
+
+            Debug.Log($"플레이어 공격 / 최종 피해 : {finalDamage}");
         }
     }
 }
