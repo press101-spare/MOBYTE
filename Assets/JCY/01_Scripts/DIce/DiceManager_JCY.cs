9b0bace84de867ed7c8170ceed37c74de1243f47
seonmulsangja <opqrstuvw0156@gmail.com>
2026-09-09 17:24:36 +0900

    폰트 만 들 ㅁ

--- Assets/JCY/01_Scripts/DIce/DiceManager_JCY.cs
+++ Assets/JCY/01_Scripts/DIce/DiceManager_JCY.cs
@@ -59,7 +59,8 @@ public class DiceManager_JCY : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            backUiPannel.SetActive(false);
+            if(backUiPannel != null)
+                 backUiPannel.SetActive(false);
             DontDestroyOnLoad(gameObject); // 씬이 넘어가도 파괴되지 않음
         }
         else
