f536b9b083070637315c01d90ea607156bbd9ec9
hanty100209-cpu <hanty100209@gmail.com>
2026-09-10 22:47:23 +0900

    ㅇㅇ

---
+++ Assets/HTY/01.Scripts/Shop/DiceType_HTY.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DiceType_HTY : MonoBehaviour
+{
+    public DiceSO_JCY _myDice;
+
+    public void SetDice(DiceSO_JCY dice)
+    {
+        _myDice = dice;
+    }
+
+    public void BuyDice()
+    {
+        //다이스를 사면됨
+    }
+}
