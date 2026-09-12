9f5584f103a0cca0a499047953ff3fa341ac71ef
hanty100209-cpu <hanty100209@gmail.com>
2026-09-12 16:46:30 +0900

    feat/ mapCommit

--- Assets/HTY/04.SO/SoScripts/GamebleSoData.cs
+++ Assets/HTY/04.SO/SoScripts/GamebleSoData.cs
@@ -4,5 +4,7 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "GamebleSoData", menuName = "Gameble/GamebleSoData")]
 public class GamebleSoData : ScriptableObject
 {
+    public string _name;
     public GameObject _panel;
+    public GameObject _object;
 }
