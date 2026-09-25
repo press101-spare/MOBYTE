8a596620fd2788bf72bf6b7cacbb3d6f0e3a7506
hanty100209-cpu <hanty100209@gmail.com>
2026-09-25 10:23:44 +0900

    feat/ 9-24

--- Assets/HTY/01.Scripts/GambleManager.cs
+++ Assets/HTY/01.Scripts/GambleManager.cs
@@ -13,17 +13,38 @@ public class GambleManager : MonoBehaviour
 
     public static GambleManager instance;
 
+    public bool _isFirst = true;
+
+    public GameObject _title;
+    public GameObject _casino;
+
     
 
     private void Awake()
     {
+        
         if(instance==null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (_isFirst)
+        {
+            _title.SetActive(true);
+            _casino.SetActive(false);
+        }
+        else
+        {
+            _title.SetActive(false);
+            _casino.SetActive(true);
         }
     }
 
@@ -51,7 +72,8 @@ public class GambleManager : MonoBehaviour
     {
         _endPanel.SetActive(true);
         _endPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text
-            = $"{_gambleData._gambleName}의 게임 결과: \n 획득배수:{x}x \n 얻은 칩:{x*_bettingChip}칩 \n 현재 보유 칩:{PlayerProfileManager.Instance.Profile.money} 칩";
+            = $"{_gambleData._gambleName}의 게임 결과: \n 획득배수:{x}x \n 얻은 칩:{x*_bettingChip}칩 \n 현재 보유 칩:{PlayerProfileManager.Instance.Profile.money+ x * _bettingChip} 칩";
+        PlayerProfileManager.Instance.Profile.money += Mathf.CeilToInt(x * _bettingChip);
 
         ResetGamble();
     }
--- Assets/Sjm/01. Script/Title Anim.cs
+++ Assets/Sjm/01. Script/Title Anim.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -14,8 +15,12 @@ public class TitleAnim : MonoBehaviour
     public RectTransform dicepo;
     public Image backGround;
     public RectTransform rotate;
+
+    public TextMeshProUGUI _touchText;
+
+    public bool _canNext;
     private void Start()
-    {   
+    {
         sequence = DOTween.Sequence();
         sequence.Append(rotate.DORotate(new Vector3(0, 0, 180), 0.4f));
         sequence.Append(title.rectTransform.DOAnchorPos(dicepo.anchoredPosition, 0.4f));
@@ -23,9 +28,17 @@ public class TitleAnim : MonoBehaviour
 
         sequence.AppendCallback(() =>
         {
-           backGround.DOFade(1f, 0.2f);
+            backGround.DOFade(1f, 0.2f);
+            _canNext = true;
         });
-       
+        sequence.AppendCallback(() =>
+        {
+            sequence = DOTween.Sequence();
+            sequence.Append(_touchText.DOFade(1, 1.5f));
+            sequence.AppendInterval(0.2f);
+            sequence.Append(_touchText.DOFade(0, 1.2f));
+            sequence.AppendInterval(0.2f);
+            sequence.SetLoops(-1);
+        });
     }
-   
 }
